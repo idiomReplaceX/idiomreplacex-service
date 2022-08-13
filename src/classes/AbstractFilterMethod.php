ca18a98ff81f3d0a4e782598258723102b2538c7
Nils Menrad <mail@nils-menrad.de>
2022-08-13 13:52:02 +0200

    Fix for private Class Problem

--- src/classes/AbstractFilterMethod.php
+++ src/classes/AbstractFilterMethod.php
@@ -2,40 +2,47 @@
 
 abstract class AbstractFilterMethod implements IFilterMethod {
 
-  protected $tokenizer;
-  protected $rpTokens = [];
+    protected $tokenizer;
+    protected $rpTokens = [];
 
-  /**
-   * @param null $text
-   */
-  public function __construct($text) {
-    $this->tokenizer  = new \tokenizer\HtmlTextTokenizer($text);
-  }
-
-  /**
-   * @return null
-   */
-  public function getText() {
-    return $this->tokenizer->getText();
-  }
-
-  /**
-   * @return \tokenizer\HtmlTextTokenizer
-   */
-  public function getTokenizer(): \tokenizer\HtmlTextTokenizer {
-    return $this->tokenizer;
-  }
-
-  /**
-   * @return array
-   */
-  public function getReplaceTokens(): array {
-    if(!$this->rpTokens){
-      $this->makeReplacementTokens();
+    /**
+     * @param null $text
+     */
+    public function __construct($text) {
+        $this->tokenizer = new \tokenizer\HtmlTextTokenizer($text);
     }
-    return $this->rpTokens;
-  }
 
-  abstract public function makeReplacementTokens();
+    /**
+     * @return null
+     */
+    public function getText() {
+        return $this->tokenizer->getText();
+    }
 
+    /**
+     * @return \tokenizer\HtmlTextTokenizer
+     */
+    public function getTokenizer(): \tokenizer\HtmlTextTokenizer {
+        return $this->tokenizer;
+    }
+
+    /**
+     * @return array
+     */
+    public function getReplaceTokens(): array {
+        if (!$this->rpTokens) {
+            $this->makeReplacementTokens();
+        }
+        $data = [];
+        foreach($this->rpTokens as $token) {
+            $arr = [];
+            $arr["replacement"] = $token->getReplacement();
+            $arr["start"] = $token->getStart();
+            $arr["token"] = $token->getToken();
+            $data[] = $arr;
+        }
+        return $data;
+    }
+
+    abstract public function makeReplacementTokens();
 }
