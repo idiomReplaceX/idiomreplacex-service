1ce90e89677bc1d673f2c8dd1956a89682a712f8
Nils Menrad <mail@nils-menrad.de>
2022-08-14 20:19:04 +0200

    New Testpattern

--- tests/OneWordFilterMethodTest.php
+++ tests/OneWordFilterMethodTest.php
@@ -6,24 +6,24 @@ final class OneWordFilterMethodTest extends TestCase
   public function testBasic(): void
   {
     
-    $filter = new OneWordFilterMethod('Tiere sind vielzellige Lebensformen, die eine Form des heterotrophen Stoff- und Energiewechsels betreiben.',FilterMethods::WEICHSPUELER);
+    $input =  "Tiere sind vielzellige Lebensformen, die eine Form des heterotrophen Stoff- und Energiewechsels betreiben."; 
+    $results = [
+        ["Lebensformen","Lebenswormen"],
+        ["heterotrophen","hederodrophen"],
+        ["Stoff-","Sdoff-"],
+        ["Energiewechsels","Energiewecbsels"],
+        ["betreiben","bedreiben"],
+    ];  
+      
+    $filter = new OneWordFilterMethod('',FilterMethods::WEICHSPUELER);
     $rpTokens = $filter->getReplaceTokens();
-    $this->assertEquals(5, count($rpTokens));
-
-    $this->assertEquals("Lebensformen", $rpTokens[0]["token"]);
-    $this->assertEquals("Lebenswormen", $rpTokens[0]["replacement"]);
-
-    $this->assertEquals("heterotrophen", $rpTokens[1]["token"]);
-    $this->assertEquals("hederodrophen", $rpTokens[1]["replacement"]);
-
-    $this->assertEquals("Stoff-", $rpTokens[2]["token"]);
-    $this->assertEquals("Sdoff-", $rpTokens[2]["replacement"]);
-
-    $this->assertEquals("Energiewechsels", $rpTokens[3]["token"]);
-    $this->assertEquals("Energiewecbsels", $rpTokens[3]["replacement"]);
     
-    $this->assertEquals("betreiben", $rpTokens[4]["token"]);
-    $this->assertEquals("bedreiben", $rpTokens[4]["replacement"]);
+    $this->assertEquals(count($results), count($rpTokens));
+    foreach ($results as $key => $value) {
+        $this->assertEquals($value[0], $rpTokens[0]["token"]);
+        $this->assertEquals($value[1], $rpTokens[0]["replacement"]);
+    }
+
   }
 
   
