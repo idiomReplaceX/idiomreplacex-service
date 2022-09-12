d96fbf37861bbd12af585be11bb5e63b31f95bf1
Andreas Kohlbecker <a.kohlbecker@bgbm.org>
2022-09-12 23:09:19 +0200

    fixing bugs

--- src/classes/FilterMethods.php
+++ src/classes/FilterMethods.php
@@ -34,26 +34,26 @@ class FilterMethods {
 
   public static function list(): array {
     $list = [
-      filterMethod(self::KACKSPECHT, "Andreas Kohlbecker", "2021", NULL),
-      filterMethod(self::LAUT, "Nils Menrad", "2022", NULL),
-      filterMethod(self::XX, "Nils Menrad", "2022", NULL),
-      filterMethod(self::HILFE, "Nils Menrad", "2022", NULL),
-      filterMethod(self::NOVERB, "Nils Menrad", "2022", NULL),
-      filterMethod(self::BASISFORM, "Nils Menrad", "2022", NULL),
-      filterMethod(self::LESEBRILLE, "Nils Menrad", "2022", NULL),
-      filterMethod(self::WEICHSPUELER, "Nils Menrad", "2022", NULL),
-      filterMethod(self::KLARSPUELER, "Nils Menrad", "2022", NULL),
-      filterMethod(self::VERNIS, "Nils Menrad", "2022", NULL),
-      filterMethod(self::POLYNESIEN, "Nils Menrad", "2022", NULL),
-      filterMethod(self::TIPPEX, "Nils Menrad", "2022", self::TIPPEX . "css"),
-      filterMethod(self::TIPPEX2, "Nils Menrad", "2022", NULL),
-      filterMethod(self::FONTSIZE, "Nils Menrad", "2022", NULL),
-      filterMethod(self::CSVREPL, "Nils Menrad", "2022", NULL)
+      FilterMethods::filterMethod(self::KACKSPECHT, "Andreas Kohlbecker", "2021", NULL),
+      FilterMethods::filterMethod(self::LAUT, "Nils Menrad", "2022", NULL),
+      FilterMethods::filterMethod(self::XX, "Nils Menrad", "2022", NULL),
+      FilterMethods::filterMethod(self::HILFE, "Nils Menrad", "2022", NULL),
+      FilterMethods::filterMethod(self::NOVERB, "Nils Menrad", "2022", NULL),
+      FilterMethods::filterMethod(self::BASISFORM, "Nils Menrad", "2022", NULL),
+      FilterMethods::filterMethod(self::LESEBRILLE, "Nils Menrad", "2022", NULL),
+      FilterMethods::filterMethod(self::WEICHSPUELER, "Nils Menrad", "2022", NULL),
+      FilterMethods::filterMethod(self::KLARSPUELER, "Nils Menrad", "2022", NULL),
+      FilterMethods::filterMethod(self::VERNIS, "Nils Menrad", "2022", NULL),
+      FilterMethods::filterMethod(self::POLYNESIEN, "Nils Menrad", "2022", NULL),
+      FilterMethods::filterMethod(self::TIPPEX, "Nils Menrad", "2022", self::TIPPEX . "css"),
+      FilterMethods::filterMethod(self::TIPPEX2, "Nils Menrad", "2022", NULL),
+      FilterMethods::filterMethod(self::FONTSIZE, "Nils Menrad", "2022", NULL),
+      FilterMethods::filterMethod(self::CSVREPL, "Nils Menrad", "2022", NULL)
     ];
     $dynamic = Db::getFilter();
     foreach ($dynamic as $dyn){
       if(!is_array($dyn)){
-       $list[] = filterMethod($dyn);
+       $list[] = FilterMethods::filterMethod($dyn);
       } else {
         $list[] = $dyn;
       }
@@ -61,7 +61,7 @@ class FilterMethods {
     return $list;
   }
 
-  function filterMethod($name, $author = NULL, $year = NULL, $cssFile = NULL): array {
+  static function filterMethod($name, $author = NULL, $year = NULL, $cssFile = NULL): array {
     return [
       "name" => $name,
       "author" => $author,
