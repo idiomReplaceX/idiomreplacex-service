629a0747d1ef5eba5e6a584754b8ab0c46a26dd6
Nils Menrad <mail@nils-menrad.de>
2022-09-15 12:10:25 +0200

    Filter Update Wort/Zeichen [Deploy: Production]

--- src/classes/ReplaceDbFilterMethod.php
+++ src/classes/ReplaceDbFilterMethod.php
@@ -7,6 +7,7 @@ class ReplaceDbFilterMethod extends AbstractFilterMethod {
 
     
     private $filter_id = NULL;
+    private $filter_art = NULL;
     
     /**
      * @return void
@@ -15,9 +16,10 @@ class ReplaceDbFilterMethod extends AbstractFilterMethod {
         
         //Get Filter ID from Filtertext
         $db = new Db();
-        $r = $db->queryRow("select id from ersetzungen_filter where filtername='".mysqli_real_escape_string($db->link,$this->getSubfilter())."'");
+        $r = $db->queryRow("select id,art from ersetzungen_filter where filtername='".mysqli_real_escape_string($db->link,$this->getSubfilter())."'");
         if(!$r) return false;
         $this->filter_id = $r["id"];
+        $this->filter_art = $r["art"];
         
         foreach ($this->getTokenizer()->getTextTokens() as $textToken) {
             $new = self::replace($textToken->getToken());
@@ -33,11 +35,30 @@ class ReplaceDbFilterMethod extends AbstractFilterMethod {
     protected function replace($wort) {
         if (!empty($wort)) {
             $db = new Db();
-            $sql = "SELECT ersatz FROM `ersetzungen` WHERE `cat` = " .$this->filter_id. " and `wort` = '" . mysqli_real_escape_string($db->link,$wort). "'";
-            $row = $db->queryRow($sql);
-            if(count($row)>0) {
-                return $row["ersatz"];
-            } 
+            
+            if($this->filter_art=="wort") {
+                $sql = "SELECT ersatz FROM `ersetzungen` WHERE `cat` = " .$this->filter_id. " and `wort` = '" . mysqli_real_escape_string($db->link,$wort). "'";
+                $row = $db->queryRow($sql);
+                if(count($row)>0) {
+                    return $row["ersatz"];
+                } 
+            }
+            
+            if($this->filter_art=="zeichen") {
+                $sql = "SELECT wort,ersatz FROM `ersetzungen` WHERE `cat` = " .$this->filter_id;
+                $result = mysqli_query($db->link,$sql);
+                $wort_org = $wort;
+                if($result) {
+                    while ($row = $result->fetch_row()) {
+                        //$list[] = $row[1]??"??";
+                        $search = $row[0];
+                        $replace = $row[1];
+                        $wort = str_replace($search, $replace, $wort);
+                    }
+                }
+                if($wort!=$wort_org) return $wort;
+            }
+            
         } else
             return false;
     }
